57b141e3a39f3b6de3656291ee8526be6ad2065d
rajudev24 <151947681+rajudevit@users.noreply.github.com>
2024-04-25 13:17:49 +0600

    update the origin url to solve CORS issue

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://inventory-client-aur1.onrender.com',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept',
     credentials: true,
